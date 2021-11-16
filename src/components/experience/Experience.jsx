4c4bc11f159fa18f0d206b0fced8e8db38916bf6
soufiane <zesoufian@gmail.Com>
2021-11-16 02:37:23 +0100

    Add experience component

---
+++ src/components/experience/Experience.jsx
@@ -0,0 +1,20 @@
+import "./experience.scss";
+
+export default function Experience() {
+    return (
+        <div className="experience" id="experience">
+            <div className="left">
+                <h2>Study</h2>
+                <div className="wrapper">
+
+                </div>
+            </div>
+            <div className="right">
+                <h2>Work</h2>
+                <div className="wrapper">
+
+                </div>
+            </div>
+        </div>
+    )
+}
