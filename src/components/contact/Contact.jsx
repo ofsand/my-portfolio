c28c2a8c91d538e8b1d8ac594c76d4ec54aa2a5e
soufiane <zesoufian@gmail.Com>
2021-11-16 02:46:06 +0100

    First build of contact component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,31 @@
 import "./contact.scss"
+import { useState } from "react";
+
 
 export default function Contact() {
+
+    const [message,setMessage] = useState(false)
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setMessage(true);
+    }
     return (
         <div className='contact' id='contact'>
-            
+            <div className="left">
+                <h2>Hire me.</h2>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder="Email"/>
+                    <textarea placeholder="Message"></textarea>
+                    <button type="submit">Send</button>
+                    {message && <span>Thank you for contacting me, I'll reply ASAP !</span>}
+                </form>
+            </div>
+
+            <div className="right">
+                <img src="assets/shake.svg" alt="" />
+            </div>
+
         </div>
     )
 }
