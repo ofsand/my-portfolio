46bb250f0c320c3a06f3b5d2b2334be59244acef
soufiane <zesoufian@gmail.Com>
2021-11-20 03:58:30 +0100

    Add waves styling in the last page wich is the contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -13,7 +13,7 @@ export default function Contact() {
     return (
         <div className='contact' id='contact'>
             <div className="left">
-                <h2>Hire me.</h2>
+                <h2>Contact</h2>
                 <form onSubmit={handleSubmit}>
                     <input type="text" placeholder="Email"/>
                     <textarea placeholder="Message"></textarea>
@@ -23,9 +23,18 @@ export default function Contact() {
             </div>
 
             <div className="right">
-                <img src="assets/shake.svg" alt="" />
+                <div className="right-up">
+                    <h3>Hire me !</h3>
+                </div>
+                <div className="right-down">
+                    <h3>Check my social media here !</h3>
+                </div>
             </div>
-
+            <div class="custom-shape-divider-bottom-1637374055">
+                <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                    <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
+                </svg>
+            </div>
         </div>
     )
 }
